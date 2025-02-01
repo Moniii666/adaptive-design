fdd7baf27b5d1a7e8907b578cde36a0dc6dd6424
Moni <monisheesh.0829@gmail.com>
2025-02-01 17:32:14 +0500

    Add files via upload

---
+++ vvv.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menuIcon');
+    const menu = document.getElementById('menu');
+    
+    menuIcon.addEventListener('click', function() {
+      menu.classList.toggle('show');
+    });
+  });
+  
